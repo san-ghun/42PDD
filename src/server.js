3cd2bc1dcaa39d274bbc4c9fbc1bb60eb3bb9a97
Sanghun <po4865@gmail.com>
2023-03-13 19:32:25 +0100

    config: Update server.js - Add some lines of comment as documentation. - Add redirect route for invalid routes.

--- src/server.js
+++ src/server.js
@@ -2,11 +2,16 @@ import express from "express";
 
 const app = express();
 
+// set templating engine
 app.set("view engine", "pug");
+// set template directory
 app.set("views", __dirname + "/views");
+// use public path locating file like JS file to be executed at FrontEnd
 app.use("/public", express.static(__dirname + "/public"));
 
+// route handler 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
