6ea9e51b0151a84404b844814beeab4c1a245c67
Sanghun <po4865@gmail.com>
2023-03-17 20:48:13 +0100

    feat: Update app.js with socketio - Connect app.js file with components in home.pug file. - Add event listener to form for submit event with handleRoomSubmit function. - Add handleRoomSubmit function to emit value to server through socket.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,43 +1,15 @@
 const socket = io();
 
-/*
-const messageList = document.querySelector("ul");
-const nicknameForm = document.querySelector("#nickname");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 
-function makeMessage(type, payload) {
-    const msg = { type, payload };
-    return JSON.stringify(msg);
-}
-
-socket.addEventListener("open", () => {
-    console.log("Connected to Server ✅");
-});
-
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server")
-});
-
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, () => {
+        console.log("server is done!");
+    });
     input.value = "";
 }
 
-function handleNicknameSubmit(event) {
-    event.preventDefault();
-    const input = nicknameForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nicknameForm.addEventListener("submit", handleNicknameSubmit);
-*/
+form.addEventListener("submit", handleRoomSubmit);
