f452a7d97a4e35f006e1a46d1b682408af8b5144
Sanghun <po4865@gmail.com>
2023-03-19 22:19:05 +0100

    feat: Show open rooms to enter
    home.pug
    - line 18-19: Add tags that show 'Open Rooms' that user can enter.
    
    app.js
    - line 66-76: Add socket on "room_change" that update the change in open rooms list.
    
    server.js
    - line 22-34: Create new function `publicRooms()` that compares the sids and rooms key value from sockets adapter, to find out the public rooms that user can connect.
    - line 45: On "enter_room", server emit to sockets the return value of `publicRooms()` which is an array of public rooms.
    - line 52-54: Add socket on "disconnect" that update array of public rooms and emit to sockets.

--- src/server.js
+++ src/server.js
@@ -19,6 +19,19 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    // const sids = wsServer.sockets.adapter.sids;
+    // const rooms = wsServer.sockets.adapter.rooms;
+    const {sockets: {adapter: {sids, rooms}}} = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anonymous";
     socket.onAny((event) => {
@@ -29,11 +42,15 @@ wsServer.on("connection", socket => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => 
             socket.to(room).emit("bye", socket.nickname)
         );
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
