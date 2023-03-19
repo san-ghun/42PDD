1e746183162ff0b297c07167c366d819ce0aaa76
Sanghun <po4865@gmail.com>
2023-03-19 23:02:22 +0100

    feat: Show user count in a room
    app.js
    - 58-60, 64-66: Update socket on "welcome" to have additional parameter `newCount` in callback to update the h3 tag showing room name and user count.
    
    server.js
    - 35-38: Create a new function `countRoom(roomName)` that return the size of a room which is count of users in the room.
    - 48, 53: Add new parameter user counts of the room to `emit` method.

--- src/server.js
+++ src/server.js
@@ -32,6 +32,10 @@ function publicRooms() {
     return publicRooms;
 }
 
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anonymous";
     socket.onAny((event) => {
@@ -41,12 +45,12 @@ wsServer.on("connection", socket => {
         socket["nickname"] = nickname;
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
         wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => 
-            socket.to(room).emit("bye", socket.nickname)
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
         );
     });
     socket.on("disconnect", () => {
