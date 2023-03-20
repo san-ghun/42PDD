d6dda76a8b92d67b46bb24741e833a957665f65e
Sanghun <po4865@gmail.com>
2023-03-20 13:32:12 +0100

    init: Initiate source code files
    - Initiate source code files to work on video call implementation with WebRTC.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import http from "http";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui" 
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -18,81 +17,7 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 // Create server 
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-    cors: {
-        origin: ["https://admin.socket.io"],
-        credentials: true,
-    },
-});
-instrument(wsServer, {
-    auth: false,
-});
-
-function publicRooms() {
-    // const sids = wsServer.sockets.adapter.sids;
-    // const rooms = wsServer.sockets.adapter.rooms;
-    const {sockets: {adapter: {sids, rooms}}} = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anonymous";
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-    });
-    socket.on("enter_room", (roomName, nickname, done) => {
-        socket["nickname"] = nickname;
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => 
-            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-        );
-    });
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-    socket.on("nickname", (nickname) => socket["nickname"] = nickname);
-})
-
-/* // ### Implementation with ws ###
-import WebSocket from "ws";
-// Create ws server on top of http server, to access and share the port
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonymous";
-    console.log("Connected to Browser ✅");
-    socket.on("close", () => console.log("Disconnected from Browser"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
-*/
+const wsServer = SocketIO(httpServer);
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
