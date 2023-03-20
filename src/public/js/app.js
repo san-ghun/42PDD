96c579b13818d8bddc92aff8898a904d0e55e96e
Sanghun <po4865@gmail.com>
2023-03-20 23:36:53 +0100

    feat: Enable client to send offer for WebRTC through server
    app.js
    - 16: Create new variable `myPeerConnection` to save peer connection value.
    - 98-102: Update a function `startMedia()` to an async function and await for `getMedia()` call, and added new function call `makeConnection()`.
    - 117-121: Update socket on "welcome" to callback async function that create offer for peer connection and emit the offer to and through server under `roomName`.
    - 124-126: Add socket on "offer" to have an argument `offer` to check and use for later.
    - 128-137: As RTC Code, create a function `makeConnection()` that create `RTCPeerConnection()` and add user's media track from the current stream to the peer connection.
    
    server.js
    - 28-30: Add socket on "offer" that accept `offer` and `roomName` as arguments and emit the `offer` to the `roomName`.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras() {
     try {
@@ -94,10 +95,11 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
     welcome.hidden = true;
     call.hidden = false;
-    getMedia();
+    await getMedia();
+    makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -112,6 +114,24 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-    console.log("someone joined the room");
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("sent the offer");
+    socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", (offer) => {
+    console.log(offer);
+})
+
+// RTC Code
+
+function makeConnection() {
+    myPeerConnection = new RTCPeerConnection();
+    myStream
+        .getTracks()
+        .forEach((track) => {
+            myPeerConnection.addTrack(track, myStream);
+        });
+}
