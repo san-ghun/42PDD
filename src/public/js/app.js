4badc486bd6f44afa08a10a0682f71e2703864ef
Sanghun <po4865@gmail.com>
2023-03-27 00:59:17 +0200

    todo: Test some features
    TODO:
    - Improve the CSS
    - Make a Chat using Data Channels
    - When a peer leaves the room, remove the stream.
    
    app.js
    - Add data channel for peer connection
    
    home.pug
    - Test pico css

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,6 +14,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -123,6 +124,9 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Code
 
 socket.on("welcome", async () => {
+  myDataChannel = myPeerConnection.createDataChannel("chat");
+  myDataChannel.addEventListener("message", (event) => console.log(event.data));
+  console.log("made data channel available");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log("sent the offer");
@@ -130,6 +134,12 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+  myPeerConnection.addEventListener("datachannel", (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener("message", (event) =>
+      console.log(event.data)
+    );
+  });
   console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
