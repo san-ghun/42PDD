6afad3b364906fb2aa3a01ffea70dbb174918ba0
Sanghun <po4865@gmail.com>
2023-04-14 17:31:30 +0200

    doc: Restructure and add code documentation

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,16 @@
-const socket = io();
+const socket = io(); // initialize socket.io connection
 
+// get DOM elements
 const call = document.getElementById("call");
-
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
 
+// set initial values
 call.hidden = true;
-
 let myStream;
 let muted = false;
 let cameraOff = false;
@@ -16,15 +18,20 @@ let roomName;
 let myPeerConnection;
 let myDataChannel;
 
+// get available cameras and populate the dropdown menu
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
     const currentCamera = myStream.getVideoTracks()[0];
+
+    // create option elements for each camera and add them to the select element
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+
+      // set the current camera as selected
       if (currentCamera.label === camera.label) {
         option.selected = true;
       }
@@ -35,6 +42,7 @@ async function getCameras() {
   }
 }
 
+// get media stream based on the given device id
 async function getMedia(deviceId) {
   const initialConstraints = {
     audio: true,
@@ -48,11 +56,14 @@ async function getMedia(deviceId) {
       height: { min: 480, ideal: 1080, max: 1080 },
     },
   };
+
   try {
     myStream = await navigator.mediaDevices.getUserMedia(
       deviceId ? cameraConstraints : initialConstraints
     );
     myFace.srcObject = myStream;
+
+    // if no device id is provided, get available cameras and populate the dropdown menu
     if (!deviceId) {
       await getCameras();
     }
@@ -61,10 +72,12 @@ async function getMedia(deviceId) {
   }
 }
 
+// handle mute button click event
 function handleMuteClick() {
   myStream
     .getAudioTracks()
     .forEach((track) => (track.enabled = !track.enabled));
+
   if (!muted) {
     muteBtn.innerText = "Unmute";
     muted = true;
@@ -74,10 +87,12 @@ function handleMuteClick() {
   }
 }
 
+// handle camera button click event
 function handleCameraClick() {
   myStream
     .getVideoTracks()
     .forEach((track) => (track.enabled = !track.enabled));
+
   if (!cameraOff) {
     cameraBtn.innerText = "Camera On";
     cameraOff = true;
@@ -87,8 +102,11 @@ function handleCameraClick() {
   }
 }
 
+// handle camera selection change event
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+
+  // if a peer connection exists, replace the video track with the new stream
   if (myPeerConnection) {
     const videoTrack = myStream.getVideoTracks()[0];
     const videoSender = myPeerConnection
@@ -98,15 +116,13 @@ async function handleCameraChange() {
   }
 }
 
+// add event listeners for mute, camera, and camera selection elements
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
 
 // Welcome Form (join a room)
-
-const welcome = document.getElementById("welcome");
-const welcomeForm = welcome.querySelector("form");
-
+// Hide the welcome form and show the call interface, then get media and create the peer connection object
 async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
@@ -114,22 +130,24 @@ async function initCall() {
   makeConnection();
 }
 
+// Handle the submission of the welcome form (joining a room)
 async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector("input");
   await initCall();
-  socket.emit("join_room", input.value);
-  roomName = input.value;
-  input.value = "";
+  socket.emit("join_room", input.value); // Send a "join_room" message to the server with the room name
+  roomName = input.value; // Save the room name in the global variable
+  input.value = ""; // Reset the input field
 }
 
+// Add an event listener to the welcome form's submit event
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
-// Socket Code
-
+// Set up socket event listeners
 socket.on("welcome", async () => {
-  myDataChannel = myPeerConnection.createDataChannel("chat");
-  myDataChannel.addEventListener("message", (event) => console.log(event.data));
+  // When the server sends a "welcome" message
+  myDataChannel = myPeerConnection.createDataChannel("chat"); // Create a new data channel named "chat"
+  myDataChannel.addEventListener("message", (event) => console.log(event.data)); // Log any incoming messages to the console
   console.log("made data channel available");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
@@ -138,12 +156,13 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+  // When the server sends an "offer" message
   myPeerConnection.addEventListener("datachannel", (event) => {
     myDataChannel = event.channel;
     myDataChannel.addEventListener("message", (event) =>
       console.log(event.data)
     );
-  });
+  }); // Set up the data channel to listen for messages
   console.log("received the offer");
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
@@ -153,17 +172,19 @@ socket.on("offer", async (offer) => {
 });
 
 socket.on("answer", (answer) => {
+  // When the server sends an "answer" message
   console.log("received the answer");
   myPeerConnection.setRemoteDescription(answer);
 });
 
 socket.on("ice", (ice) => {
+  // When the server sends an "ice" message
   console.log("received candidate");
   myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC Code
-
+// Creates a new RTCPeerConnection with the given iceServers configuration
 function makeConnection() {
   myPeerConnection = new RTCPeerConnection({
     iceServers: [
@@ -184,11 +205,13 @@ function makeConnection() {
   });
 }
 
+// Emits the "ice" event with the candidate and roomName data to the socket.
 function handleIce(data) {
   console.log("sent candidate");
   socket.emit("ice", data.candidate, roomName);
 }
 
+// Sets the srcObject of the peersFace element to the data stream.
 function handleAddStream(data) {
   const peersFace = document.getElementById("peersFace");
   peersFace.srcObject = data.stream;
